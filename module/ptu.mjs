3f65818e2b718aadf1bfcffa95314a8eb02042dc
Wandering-Magi <Wandering_Magi@hotmail.com>
2024-06-13 07:26:15 -0600

    trying to get dataModules working

--- module/ptu.mjs
+++ module/ptu.mjs
@@ -9,42 +9,49 @@
 */
 // Toast 
 // Import document classes.
-import { PTUActor } from "./documents/actor.mjs";
+// import { PTUActor } from "./documents/actor.mjs";
+import { TrainerData } from "./documents/actor.mjs"
 // Import sheet classes.
 
 // Import helper/utilitie classes and constants
-import { PTU } from ".helpers/config.mjs";
+// import { PTU } from ".helpers/config.mjs";
 
 /* -------------------------------------------- */
 /*  Init Hook                                   */
 /* -------------------------------------------- */
 
-Hooks.once("init", function () {
-  // Add utility classes to the global game object so that they're more easily accessible in global contexts
-  game.ptu = {
-    ptuTrainer,
-  };
+Hooks.once("init", () => {
+  console.log("MAGI | INIT HOOK")
+//   // Add utility classes to the global game object so that they're more easily accessible in global contexts
+//   game.ptu = {
+//     ptuTrainer,
+//   };
+// 
+//   // Add custom constants for configuration
+//   CONFIG.PTU = PTU;
+// 
+//   /**
+//    * Set an initiative formula for the system
+//    * @type {String}
+//    */
+//   CONFIG.Combat.initiative = {
+//     formula: "1d20",
+//     decimals: 2
+//   };
+// 
+//   // Define custom document classes
+//   CONFIG.Actor.documentClass = PTUActor;
+//   CONFIG.Actor.dataModels = {
+//     trainer: models.ptuTrainer,
+//     pokemon: models.ptuPokemon,
+//     npc: models.ptuNPC,
+//   };
+//
 
-  // Add custom constants for configuration
-  CONFIG.PTU = PTU;
-
-  /**
-   * Set an initiative formula for the system
-   * @type {String}
-   */
-  CONFIG.Combat.initiative = {
-    formula: "1d20",
-    decimals: 2
-  };
-
-  // Define custom document classes
-  CONFIG.Actor.documentClass = PTUActor;
-  CONFIG.Actor.dataModels = {
-    trainer: models.ptuTrainer,
-    pokemon: models.ptuPokemon,
-    npc: models.ptuNPC,
-  };
-
+  console.log("Assigning trainers"),
+  Object.assign(CONFIG.Actor.dataModels, {
+    Trainer: TrainerData,
+  })
 });
 
 /* -------------------------------------------- */
@@ -62,4 +69,6 @@ Hooks.once("init", function () {
 
 Hooks.once("ready", function() {
   // Include steps that need to happen after Foundry has fully loaded here.
+  console.log("MAGI | READY HOOK")
+
 }); 
